aaa05a9b064f27305aef4879cbb0aa10a9c3b0f2
Bartosz Kosiński <bkosinski@corpnet.local>
2024-01-11 00:52:56 +0100

    debugging

--- packetbaseml/src/models/operations.py
+++ packetbaseml/src/models/operations.py
@@ -26,20 +26,36 @@ def load_model(model_name: str) -> GenericModel:
 
 
 def precision(true_predictions: int, false_positive: int) -> float:
-    precision: float = 100.0 * true_predictions / (true_predictions + false_positive)
+    try:
+        precision: float = (
+            100.0 * true_predictions / (true_predictions + false_positive)
+        )
+    except:
+        precision = 0.0
     return precision
 
 
 def accuracy(true_predictions: int, predicitons_amount: int) -> float:
-    accuracy: float = 100.0 * true_predictions / predicitons_amount
+    try:
+        accuracy: float = 100.0 * true_predictions / predicitons_amount
+    except:
+        accuracy = 0.0
     return accuracy
 
 
 def sensitivity(true_predictions: int, false_negative: int) -> float:
-    sensitivity: float = 100.0 * true_predictions / (true_predictions + false_negative)
+    try:
+        sensitivity: float = (
+            100.0 * true_predictions / (true_predictions + false_negative)
+        )
+    except:
+        sensitivity = 0.0
     return sensitivity
 
 
 def f1(precision: float, sensitivity: float) -> float:
-    f1: float = 2 * precision * sensitivity / (precision + sensitivity)
+    try:
+        f1: float = 2 * precision * sensitivity / (precision + sensitivity)
+    except:
+        f1 = 0.0
     return f1
